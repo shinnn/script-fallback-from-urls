96a7222fa904835973804801f87eadad5db749a9
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-01-24 23:05:27 +0900

    use minimist options and introduce sum-up
    https://github.com/shinnn/sum-up

--- cli.js
+++ cli.js
@@ -1,45 +1,49 @@
 'use strict';
 
-var argv = require('minimist')(process.argv.slice(2));
-var pkg = require('./package.json');
+var argv = require('minimist')(process.argv.slice(2), {
+  alias: {
+    'var': 'variable',
+    'V': 'variable',
+    'h': 'help',
+    'v': 'version'
+  },
+  string: ['_'],
+  boolean: ['help', 'version']
+});
 
-function help() {
-  var chalk = require('chalk');
+if (argv.version) {
+  console.log(require('./package.json').version);
+
+} else if (argv.help || argv._.length === 0) {
+  var sumUp = require('sum-up');
+  var yellow = require('chalk').yellow;
+
+  var pkg = require('./package.json');
 
   console.log([
-    chalk.cyan(pkg.name) + chalk.gray(' v' + pkg.version),
-    pkg.description,
+    sumUp(pkg),
     '',
     'Usage: ' + pkg.name + ' <url1> <url2> [<url3> ...] --variable <variable>',
     '',
-    chalk.yellow('--variable, --var, -V') + '  Specify a required global variable',
+    yellow('--variable, --var, -V') + '  Specify a required global variable',
     '',
     'Options:',
-    chalk.yellow('--no-min,            ') + '  Do not minify output',
-    chalk.yellow('--help,            -h') + '  Print usage information',
-    chalk.yellow('--version,         -v') + '  Print version',
+    yellow('--no-min,            ') + '  Do not minify output',
+    yellow('--help,            -h') + '  Print usage information',
+    yellow('--version,         -v') + '  Print version',
     ''
   ].join('\n'));
-}
 
-var variable = argv.var || argv.variable || argv.V;
-
-if (argv.version || argv.v) {
-  console.log(pkg.version);
-} else if (argv.help || argv.h || argv._.length === 0) {
-  help();
 } else if (argv._.length < 2) {
   process.stderr.write('More than 2 URLs required.\n', function() {
     process.exit(1);
   });
-} else if (variable === undefined || variable === true) {
+
+} else if (argv.variable === undefined || argv.variable === true) {
   process.stderr.write('--variable <variable> required.\n', function() {
     process.exit(1);
   });
-} else {
-  var urls = argv._.map(function(el) {
-    return '' + el;
-  });
 
-  console.log(require('./' + pkg.main)(variable, urls, {min: argv.min}));
+} else {
+  console.log(require('./')(argv.variable, argv._, {min: argv.min}));
 }
