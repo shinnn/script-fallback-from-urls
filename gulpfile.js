a3cde60de0b655bf6b9a983d03075b70065d59c7
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-01-24 23:08:09 +0900

    simplify gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -12,20 +12,15 @@ var toCamelCase = require('to-camel-case');
 
 var pkg = require('./package.json');
 var bower = require('./bower.json');
+var banner = require('tiny-npm-license')(pkg);
 var funName = toCamelCase(pkg.name);
 
-var banner = [
-  '/*!',
-  ' * <%= pkg.name %>.js | MIT (c) Shinnosuke Watanabe',
-  ' * <%= pkg.homepage %>',
-  '*/\n'
-].join('\n');
-
 gulp.task('lint', function() {
-  gulp.src(['{,src/}*.js'])
+  gulp.src('{,src/}*.js')
+    .pipe($.jscs(pkg.jscsConfig))
     .pipe($.jshint())
     .pipe($.jshint.reporter(stylish))
-    .pipe($.jscs('.jscs.json'));
+    .pipe($.jshint.reporter('fail'));
   gulp.src('*.json')
     .pipe($.jsonlint())
     .pipe($.jsonlint.reporter());
@@ -35,32 +30,30 @@ gulp.task('clean', rimraf.bind(null, 'dist'));
 
 gulp.task('build', ['lint', 'clean'], function() {
   return mergeStream(
-    gulp.src(['src/*.js'])
-      .pipe($.header(banner + '!function() {\n', {pkg: pkg}))
-      .pipe($.footer('\nwindow.<%= funName %> = <%= funName %>;\n}();\n', {
-        funName: funName
-      }))
-      .pipe($.rename(bower.main))
-      .pipe(gulp.dest('')),
-    gulp.src(['src/*.js'])
-      .pipe($.header(banner, {pkg: pkg}))
-      .pipe($.footer('\nmodule.exports = <%= funName %>;\n', {funName: funName}))
+    gulp.src('src/*.js')
+      .pipe($.header(banner + '!function() {\n'))
+      .pipe($.footer('\nwindow.' + funName + ' = ' + funName + ';\n}();\n'))
+      .pipe($.rename(bower.main)),
+    gulp.src('src/*.js')
+      .pipe($.header(banner))
+      .pipe($.footer('\nmodule.exports = ' + funName + ';\n'))
       .pipe($.rename(pkg.main))
-      .pipe(gulp.dest(''))
-  );
+  )
+    .pipe($.size({showFiles: true}))
+    .pipe(gulp.dest(''));
 });
 
 gulp.task('test', ['build'], function(cb) {
   exec('node test.js', function(err, stderr, stdout) {
-    console.log(stderr);
-    console.log(stdout);
+    process.stdout.write(stderr);
+    process.stderr.write(stdout);
     cb(err);
   });
 });
 
 gulp.task('watch', function() {
-  gulp.watch(['{,src/}*.js'], ['test']);
-  gulp.watch(['*.json', '.jshintrc'], ['lint']);
+  gulp.watch('{,src/}*.js', ['test']);
+  gulp.watch('{*.json,.jshintrc}', ['lint']);
 });
 
 gulp.task('default', ['test', 'watch']);
